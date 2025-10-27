b53e89ee14dc64731c6eb3bb79351dd9aacd4341
ynnmuraii <tihookeanski.y.xxtel@gmail.com>
2025-10-27 17:27:05 +0400

    add CrudEndpoints for AircraftFamily and intergrate swagger

--- Airline.API/Program.cs
+++ Airline.API/Program.cs
@@ -14,4 +14,37 @@ app.UseSwaggerUI();
 
 app.MapGet("/", () => "Airline API работает");
 
+app.MapGet("/api/families", (AirlineData db) => db.Families);
+app.MapGet("/api/families/{id:int}", (AirlineData db, int id) =>
+{
+    var fam = db.Families.FirstOrDefault(f => f.Id == id);
+    return fam is not null ? Results.Ok(fam) : Results.NotFound();
+});
+
+app.MapPost("/api/families", (AirlineData db, AircraftFamily family) =>
+{
+    family.Id = db.Families.Max(f => f.Id) + 1;
+    db.Families.Add(family);
+    return Results.Created($"/api/families/{family.Id}", family);
+});
+
+app.MapPut("/api/families/{id:int}", (AirlineData db, int id, AircraftFamily input) =>
+{
+    var fam = db.Families.FirstOrDefault(f => f.Id == id);
+    if (fam is null) return Results.NotFound();
+
+    fam.Manufacturer = input.Manufacturer;
+    fam.FamilyName = input.FamilyName;
+    return Results.Ok(fam);
+});
+
+app.MapDelete("/api/families/{id:int}", (AirlineData db, int id) =>
+{
+    var fam = db.Families.FirstOrDefault(f => f.Id == id);
+    if (fam is null) return Results.NotFound();
+
+    db.Families.Remove(fam);
+    return Results.NoContent();
+});
+
 app.Run();
